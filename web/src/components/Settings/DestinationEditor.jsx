2e19e72dc909c5bed78b75179045e0322917e8df
rmfatemi <rmfatemi@gmail.com>
2025-05-31 00:27:47 -0400

    add destination reference guide

--- web/src/components/Settings/DestinationEditor.jsx
+++ web/src/components/Settings/DestinationEditor.jsx
@@ -3,9 +3,9 @@ import YamlEditor from './YamlEditor';
 
 function DestinationEditor({ destinations, updateConfig }) {
     const destinationsReference =
-`- name: 'Telegram'
-  enabled: false
-  url: 'tgram://{token}/{chat_id}'`;
+`- name: 'Telegram'                    # application name (refer to https://github.com/caronc/apprise/wiki for supported applications and how to configure them)
+  enabled: false                      # whether destination should be disabled (global setting, overrides custom rules)
+  url: 'tgram://{token}/{chat_id}'    # apprise compatible url with tokens e.g. for telegram refer to (https://github.com/caronc/apprise/wiki/Notify_telegram)`;
 
     const handleDestinationsUpdate = (updatedDestinations) => {
         updateConfig(prev => ({
